7497b80b5b882ecad2f08f58aedc71581d086857
jakelevi1996 <jakelevi@hotmail.co.uk>
2024-02-01 23:56:15 +0000

    Use correct case for `src` directory

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,6 +3,6 @@ import sys
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, ".."))
-SRC_DIR = os.path.join(ROOT_DIR, "SRC")
+SRC_DIR = os.path.join(ROOT_DIR, "src")
 
 sys.path.append(SRC_DIR)
